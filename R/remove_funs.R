85f5ea2a246fcaeff59c8e82d6d6ee5e2608b3d1
Sebastian Weirich <sebastian.weirich@iqb.hu-berlin.de>
2024-04-05 15:13:09 +0000

    version 0.7.6

--- R/remove_funs.R
+++ R/remove_funs.R
@@ -3,13 +3,19 @@ removeNonNumeric <- function ( string ) {gsub("[^0-9]","",string)}
 
 ### entfernt bestimmtes Pattern aus einem String
 removePattern     <- function ( string, pattern ) {
+  checkmate::assert_character(string)
+  checkmate::assert_character(pattern, len = 1)
   splitt <- strsplit(string, pattern)
   ret    <- unlist(lapply(splitt, FUN = function ( y ) { paste(y, collapse="")}))
   return(ret)}
 
-removeNumeric <- function ( string ) {gsub("0|1|2|3|4|5|6|7|8|9","",string)}
+removeNumeric <- function ( string ) {
+  checkmate::assert_character(string)
+  gsub("0|1|2|3|4|5|6|7|8|9","",string)}
 
 crop <- function ( x , char = " " ) {
+  checkmate::assert_character(x)
+  checkmate::assert_vector(char, len = 1, strict = TRUE)
   if ( char %in% c ( "\\" , "+" , "*" , "." , "(" , ")" , "[" , "]" , "{" , "}" , "|" , "^" , "$" ) ) {char <- paste ( "\\" , char , sep = "" ) }
   gsub ( paste ( "^" , char , "+|" , char , "+$" , sep = "" ) , "" , x ) }
 
@@ -26,18 +32,22 @@ gsubAll <- function(string, old, new) {
 
 ### splits the string only on the first or the last occurrence of the separator
 halveString <- function (string, pattern, first = TRUE, colnames=c("X1", "X2") )  {
+  checkmate::assert_character(string)
+  checkmate::assert_character(pattern, len = 1)
+  checkmate::assert_logical(first, len = 1)
   if( !(inherits(colnames, "character") && length(colnames)==2 && length(unique(colnames)) == 2 )) {stop("'colnames' must be a unique character vector of length 2.")}
   if(!first) {
-     front<- sub(paste0(pattern, "[^",pattern,"]+$"), "", string)
-     back <- sub(paste0(".*",pattern,"|^[^",pattern,"]*$"), "", string)
-     back[back==""] <- NA
+     ret <- rbind_fill_vector(regmatches(stringi::stri_reverse(string), regexpr(pattern, stringi::stri_reverse(string)), invert = TRUE))
+     for ( i in 1:ncol(ret)) {ret[,i] <- stringi::stri_reverse(ret[,i])}
+     na  <- which(apply(ret, MARGIN = 1, FUN = function(y) {any(is.na(y))}))    ### reverse muss wieder rueckgaengig gemacht werden, ausser fuer die NAs
+     ret <- ret[,c(2,1), drop=FALSE]                                            ### die sollen an der Stelle bleiben, wo sie sind
+     if(length(na)>0) {ret[na,] <- ret[na,c(2,1)]}                              ### falls es welche gibt, muss das rueckgaengig wieder rueckgaengig gemacht werden
   }
   if(first) {
-     front<- gsub(paste0(pattern, ".*"),"",string)
-     back <- sub(paste0("^([^",pattern,"]+)",pattern), "", string)
-     len1 <- which(unlist(lapply(strsplit(x=string, split=pattern), length))==1)
-     if(length(len1)>0) {back[len1] <- NA}
+     ret <- rbind_fill_vector(regmatches(string, regexpr(pattern, string), invert = TRUE))
   }
-  ret <- matrix(c(front, back), ncol=2, dimnames=list(NULL, colnames))
+  ret[ret==""] <- NA                                                            ### keine leeren Strings, sondern NAs
+  colnames(ret) <- colnames
   return(ret)}
+
 
--- tests/testthat/test_remove_funs.R
+++ tests/testthat/test_remove_funs.R
@@ -29,7 +29,7 @@ test_that("halveString1", {
   expect_equal(dim(d1), c(2,2))
   expect_equal(d1[,1], rep("a", 2))
   expect_equal(d1[,2], c("b__c", "_m___k__"))
-  expect_equal(d2[,1], c("a__b" ,"a___m___k__"))
+  expect_equal(d2[,1], c("a__b" ,"a___m___k"))
   expect_equal(d2[,2], c("c", NA))
 })
 
@@ -41,9 +41,28 @@ test_that("halveString2", {
   expect_equal(dim(d4), c(2,2))
   expect_equal(d3[,1], rep("a", 2))
   expect_equal(d3[,2], c("b.c", "m.k."))
-  expect_equal(d4[,1], c("a.b" ,"a.m.k."))
+  expect_equal(d4[,1], c("a.b" ,"a.m.k"))
   expect_equal(d4[,2], c("c", NA))
 })
 
+
+paths <- c("BB/DHW/LV2011_MA_PV-Ziehung_ohneKBimHGM_BB__DHW__ANCHOR.pvl", "BB/GL/LV2011_MA_PV-Ziehung_ohneKBimHGM_BB__GL__ANCHOR.pvl",  "BB/GM/LV2011_MA_PV-Ziehung_ohneKBimHGM_BB__GM__ANCHOR.pvl")
+test_that("halveString3", {
+  d1  <- halveString(paths, pattern = "__", colnames= c("a", "b"))
+  expect_equal(d1[,"b"], c("DHW__ANCHOR.pvl", "GL__ANCHOR.pvl",  "GM__ANCHOR.pvl"))
+  expect_equal(d1[,"a"], c("BB/DHW/LV2011_MA_PV-Ziehung_ohneKBimHGM_BB", "BB/GL/LV2011_MA_PV-Ziehung_ohneKBimHGM_BB", "BB/GM/LV2011_MA_PV-Ziehung_ohneKBimHGM_BB"))
+})
+
+test_that("halveString4", {
+  d1  <- halveString(paths, pattern = "__", first=FALSE, colnames= c("a", "b"))
+  expect_equal(d1[,"b"], rep("ANCHOR.pvl", 3))
+  expect_equal(d1[,"a"], c("BB/DHW/LV2011_MA_PV-Ziehung_ohneKBimHGM_BB__DHW", "BB/GL/LV2011_MA_PV-Ziehung_ohneKBimHGM_BB__GL","BB/GM/LV2011_MA_PV-Ziehung_ohneKBimHGM_BB__GM"))
+})
+
+test_that("halveString5", {
+  d1  <- halveString(paths, pattern = "/", first=FALSE, colnames= c("a", "b"))
+  expect_equal(d1[,"b"], c("LV2011_MA_PV-Ziehung_ohneKBimHGM_BB__DHW__ANCHOR.pvl", "LV2011_MA_PV-Ziehung_ohneKBimHGM_BB__GL__ANCHOR.pvl",  "LV2011_MA_PV-Ziehung_ohneKBimHGM_BB__GM__ANCHOR.pvl"))
+  expect_equal(d1[,"a"], c("BB/DHW", "BB/GL", "BB/GM" ))
+})
 
 
